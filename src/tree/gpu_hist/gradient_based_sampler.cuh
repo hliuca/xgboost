495816f6945ffd7057a476867c9d3598eb9fc94f
amdsc21 <96135754+amdsc21@users.noreply.github.com>
2023-03-09 22:26:08 +0100

    finished gradient_based_sampler.cu

--- src/tree/gpu_hist/gradient_based_sampler.cuh
+++ src/tree/gpu_hist/gradient_based_sampler.cuh
@@ -6,7 +6,12 @@
 #include <xgboost/data.h>
 #include <xgboost/span.h>
 
+#if defined(XGBOOST_USE_CUDA)
 #include "../../common/device_helpers.cuh"
+#elif defined(XGBOOST_USE_HIP)
+#include "../../common/device_helpers.hip.h"
+#endif
+
 #include "../../data/ellpack_page.cuh"
 
 namespace xgboost {
