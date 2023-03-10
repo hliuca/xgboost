8fd2af1c8bfc481935ee8abbc4985993c3b8e856
amdsc21 <96135754+amdsc21@users.noreply.github.com>
2023-03-10 05:16:23 +0100

    finish numeric.cu

--- src/common/numeric.cu
+++ src/common/numeric.cu
@@ -3,7 +3,12 @@
  */
 #include <thrust/execution_policy.h>
 
+#if defined(XGBOOST_USE_CUDA)
 #include "device_helpers.cuh"            // dh::Reduce, dh::XGBCachingDeviceAllocator
+#elif defined(XGBOOST_USE_HIP)
+#include "device_helpers.hip.h"          // dh::Reduce, dh::XGBCachingDeviceAllocator
+#endif
+
 #include "numeric.h"
 #include "xgboost/context.h"             // Context
 #include "xgboost/host_device_vector.h"  // HostDeviceVector
@@ -15,8 +20,14 @@ double Reduce(Context const* ctx, HostDeviceVector<float> const& values) {
   values.SetDevice(ctx->gpu_id);
   auto const d_values = values.ConstDeviceSpan();
   dh::XGBCachingDeviceAllocator<char> alloc;
+
+#if defined(XGBOOST_USE_CUDA)
   return dh::Reduce(thrust::cuda::par(alloc), dh::tcbegin(d_values), dh::tcend(d_values), 0.0,
                     thrust::plus<float>{});
+#elif defined(XGBOOST_USE_HIP)
+  return dh::Reduce(thrust::hip::par(alloc), dh::tcbegin(d_values), dh::tcend(d_values), 0.0,
+                    thrust::plus<float>{});
+#endif
 }
 }  // namespace cuda_impl
 }  // namespace common
