5edfc1e2e9952f5c92fd9e410d7b422b377804b8
amdsc21 <96135754+amdsc21@users.noreply.github.com>
2023-03-10 06:41:25 +0100

    finish ellpack_page.cc

--- src/data/ellpack_page.cc
+++ src/data/ellpack_page.cc
@@ -1,7 +1,7 @@
 /*!
  * Copyright 2019 XGBoost contributors
  */
-#if !defined(XGBOOST_USE_CUDA)
+#if !defined(XGBOOST_USE_CUDA) && !defined(XGBOOST_USE_HIP)
 
 #include <xgboost/data.h>
 
@@ -28,39 +28,6 @@ void EllpackPage::SetBaseRowId(std::size_t) {
 }
 size_t EllpackPage::Size() const {
   LOG(FATAL) << "Internal Error: XGBoost is not compiled with CUDA but "
-                "EllpackPage is required";
-  return 0;
-}
-
-}  // namespace xgboost
-
-#elif !defined(XGBOOST_USE_HIP)
-
-#include <xgboost/data.h>
-
-// dummy implementation of EllpackPage in case HIP is not used
-namespace xgboost {
-
-class EllpackPageImpl {};
-
-EllpackPage::EllpackPage() = default;
-
-EllpackPage::EllpackPage(DMatrix*, const BatchParam&) {
-  LOG(FATAL) << "Internal Error: XGBoost is not compiled with HIP but "
-                "EllpackPage is required";
-}
-
-EllpackPage::~EllpackPage() {
-  LOG(FATAL) << "Internal Error: XGBoost is not compiled with HIP but "
-                "EllpackPage is required";
-}
-
-void EllpackPage::SetBaseRowId(std::size_t) {
-  LOG(FATAL) << "Internal Error: XGBoost is not compiled with HIP but "
-                "EllpackPage is required";
-}
-size_t EllpackPage::Size() const {
-  LOG(FATAL) << "Internal Error: XGBoost is not compiled with HIP but "
                 "EllpackPage is required";
   return 0;
 }
