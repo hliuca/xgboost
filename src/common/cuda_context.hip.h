c9c6e0b1af7e586aea5145a4c9cab201e4e818f5
amdsc21 <96135754+amdsc21@users.noreply.github.com>
2023-03-27 19:36:27 +0200

    add sep *.hip and *.hip.h

---
+++ src/common/cuda_context.hip.h
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2022 by XGBoost Contributors
+ */
+#ifndef XGBOOST_COMMON_CUDA_CONTEXT_CUH_
+#define XGBOOST_COMMON_CUDA_CONTEXT_CUH_
+#include <thrust/execution_policy.h>
+#include "device_helpers.hip.h"
+
+namespace xgboost {
+struct CUDAContext {
+ private:
+  dh::XGBCachingDeviceAllocator<char> caching_alloc_;
+  dh::XGBDeviceAllocator<char> alloc_;
+
+ public:
+  /**
+   * \brief Caching thrust policy.
+   */
+#if defined(XGBOOST_USE_HIP)
+  auto CTP() const { return thrust::hip::par(caching_alloc_).on(dh::DefaultStream()); }
+#else
+  auto CTP() const { return thrust::cuda::par(caching_alloc_).on(dh::DefaultStream()); }
+#endif
+
+  /**
+   * \brief Thrust policy without caching allocator.
+   */
+#if defined(XGBOOST_USE_HIP)
+  auto TP() const { return thrust::hip::par(alloc_).on(dh::DefaultStream()); }
+#else
+  auto TP() const { return thrust::cuda::par(alloc_).on(dh::DefaultStream()); }
+#endif
+
+  auto Stream() const { return dh::DefaultStream(); }
+};
+}  // namespace xgboost
+#endif  // XGBOOST_COMMON_CUDA_CONTEXT_CUH_
